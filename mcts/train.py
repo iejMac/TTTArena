60a834b71b37941d9567d232b54b66d1eb5b1ced
iejMac <kilianmaciej6@gmail.com>
2021-06-02 21:34:39 +0200

    no print

--- mcts/train.py
+++ mcts/train.py
@@ -1,14 +1,14 @@
 from model import ZeroTTT
 
-model = ZeroTTT(brain_path='best_model', opt_path='best_opt_state', lr=3e-4, weight_decay=2e-4, board_len=10)
-# model = ZeroTTT(brain_path=None, opt_path=None, lr=3e-4, weight_decay=2e-4, board_len=10)
+# model = ZeroTTT(brain_path='best_model', opt_path='best_opt_state', lr=3e-4, weight_decay=2e-4, board_len=10)
+model = ZeroTTT(brain_path=None, opt_path=None, lr=3e-4, weight_decay=2e-4, board_len=10)
 
 print(model.get_parameter_count())
 print(sum(p.numel() for p in model.brain.policy_head.parameters() if p.requires_grad))
 print(sum(p.numel() for p in model.brain.value_head.parameters() if p.requires_grad))
 
-model.self_play(n_games=1000, num_simulations=200, render=1, training_epochs=1, min_positions_learn=300000, positions_per_learn=100000, batch_size=40)
-# model.self_play(n_games=1000, num_simulations=100, render=1, training_epochs=1, min_positions_learn=300, positions_per_learn=100, batch_size=40)
+# model.self_play(n_games=1000, num_simulations=200, render=1, training_epochs=1, min_positions_learn=300000, positions_per_learn=100000, batch_size=40)
+model.self_play(n_games=1000, num_simulations=100, render=1, training_epochs=1, min_positions_learn=300, positions_per_learn=100, batch_size=40)
 
 '''
   Bugs:
