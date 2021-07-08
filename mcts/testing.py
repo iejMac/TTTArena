a920fb1f8a67ca2a2cb0e2dc676ed0d35e9dc850
iejMac <kilianmaciej6@gmail.com>
2021-07-08 07:36:10 +0000

    new model wow

--- mcts/testing.py
+++ mcts/testing.py
@@ -166,13 +166,13 @@ class Test:
     self.env.reset()
 
 mcts_args = {
-  "num_simulations": 800,
+  "num_simulations": 200,
   "alpha": 0.1,
   "dirichlet_alpha": 0.3,
   "c_puct": 4
 }
     
-test = Test("trained_model_0_1", "trained_opt_state_0_1", 10) 
+test = Test("trained_model_1_0", "trained_opt_state_1_0", 10) 
 
 pos1 = [(5, 5), (4, 5), (4, 4), (3, 6), (4, 6), (3, 5), (2, 6), (3, 7), (2, 7), (3, 4),
 (3, 3), (2, 5), (3, 8), (1, 5), (0, 5), (1, 4), (2, 2)]
@@ -182,8 +182,8 @@ pos4 = [(0, 0), (6, 5), (0, 1), (8, 4), (0, 2), (5, 9), (0, 3), (5, 6)]
 pos5 = [(3, 4), (5, 5), (3, 5), (5, 4), (3, 6), (4, 4), (3, 7), (4, 5), (3, 8)]
 
 # test.human_game_evaluation("../data/30x30")
-# test.compare_model("trained_model_0_0", "trained_opt_state_0_0", 40, render=1, mcts_args=mcts_args)
-test.play_model(player="O", mcts_args=mcts_args)
+test.compare_model("trained_model_1", "trained_opt_state_1", 40, render=1, mcts_args=mcts_args)
+# test.play_model(player="O", mcts_args=mcts_args)
 
 if False:
     for i in range(5):
