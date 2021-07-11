d76b41aa25fae6103181aab35db19cc1d5bf0d93
bartekx43 <ba.bardian@gmail.com>
2021-07-11 16:10:42 +0200

    window added

---
+++ game.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode([800, 500])
+
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      tunning = False
+
+  screen.fill((0, 0, 0))
+
+  pygame.draw.rect(screen, (255, 255, 255), (5, 5, 20, 10))
+
+  pygame.display.flip()
+
+pygame.quit()
