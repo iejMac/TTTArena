056636f064769c3251fb2448e7487b4fa8394733
iejMac <kilianmaciej6@gmail.com>
2021-10-25 20:54:58 -0700

    random: tabs

--- random/agent.py
+++ random/agent.py
@@ -2,12 +2,12 @@ from agent import Agent
 from numpy.random import randint
 
 class RandomAgent(Agent):
-	def __init__(self, name):
-		super().__init__(name)
+  def __init__(self, name):
+    super().__init__(name)
 
-	def make_action(self, state):
-		movex = randint(0, state.shape[1])
-		movey = randint(0, state.shape[0])
-		return (movey, movex)
+  def make_action(self, state):
+    movex = randint(0, state.shape[1])
+    movey = randint(0, state.shape[0])
+    return (movey, movex)
 
 
